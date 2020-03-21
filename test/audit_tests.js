5cb1a47045299903b57c69658d07340432b2a5bd
Lee Hull <lhull@hcpci.com>
2020-03-21 12:55:55 -0400

    added more tests

--- test/audit_tests.js
+++ test/audit_tests.js
@@ -6,7 +6,7 @@ const Audit = require('../lib/audit.js');
 
 describe('Audit Tests', function () {
 
-  describe('npm audit tests', function () {
+  describe('data tests', function () {
     const testCases = [
       {
         args: { file: 'none.json', severity: 'low', json: true },
@@ -89,7 +89,50 @@ describe('Audit Tests', function () {
             assert.equal(data.metadata.high, test.expected.metadata.high);
             assert.equal(data.metadata.critical, test.expected.metadata.critical);
           });
+        } else {
+          it('get() should have a value', () => {
+            const data = this.audit.get();
+
+            assert.ok(data);
+          });
         }
+      });
+    });
+  });
+
+  describe('summary tests', function() {
+    const testCases = [
+      {
+        args: { file: 'single-low.json', severity: 'low', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 1, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 3, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true, whitelist: 'debug' },
+        expected: { code: 0, metadata: { info: 0, low: 2, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      }
+    ];
+
+    _.forEach(testCases, (test) => {
+      describe(`${test.args.file} - ${test.args.severity} ${test.args.whitelist ? test.args.whitelist : ''}`, function () {
+        before(() => {
+          this.audit = new Audit(test.args.severity, test.args.json, test.args.whitelist);
+
+          let data = fs.readFileSync(__dirname + '/data/' + test.args.file);
+          this.audit.load(data);
+        });
+
+        it('getSummary() should have correct summary', () => {
+          const data = this.audit.getSummary(test.color);
+
+          assert.equal(data, `Found ${test.expected.metadata.info} info, ${test.expected.metadata.low} low, ${test.expected.metadata.moderate} moderate, ${test.expected.metadata.high} high, ${test.expected.metadata.critical} critical vulnerabilities.`);
+        });        
       });
     });
   });
