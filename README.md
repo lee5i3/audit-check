e91936cc91ac415647d8bc27957db402734ed238
Lee Hull <lee5i3@leehull.com>
2020-03-21 13:06:50 -0400

    1.0.1 (#5)
    * Changes unrelated to a task
    
    * added short option
    
    * Changes unrelated to a task
    
    * Changes unrelated to a task
    
    * Changes unrelated to a task
    
    * Changes unrelated to a task
    
    * added eslint and coverage options
    
    * version change
    
    * added more tests

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 # audit-check
 
-## Badges  
+### Badges  
 [![Build Status](https://travis-ci.org/lee5i3/audit-check.svg?branch=master)](https://travis-ci.org/lee5i3/audit-check)
 [![codecov](https://codecov.io/gh/lee5i3/audit-check/branch/master/graph/badge.svg)](https://codecov.io/gh/lee5i3/audit-check)
 [![Known Vulnerabilities](https://snyk.io/test/github/lee5i3/audit-check/badge.svg)](https://snyk.io/test/github/lee5i3/audit-check)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=lee5i3_audit-check&metric=alert_status)](https://sonarcloud.io/dashboard?id=lee5i3_audit-check)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/f91b80021c6d4f0cb4f1dda56f580273)](https://www.codacy.com/manual/lee5i3/audit-check?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=lee5i3/audit-check&amp;utm_campaign=Badge_Grade)
 
-# CLI
+### Install
 ```
-Usage: audit-check [options]
+npm install -g audit-check
+```
 
-Options:
-  -s, --severity <severity>  severity to fail on (default: "info")
-  -j --json                  output json
-  -i, --ignore-dev           ignore dev dependencies
-  -h, --help                 display help for command
+### Common Configuration Options
+| Options | Description  | Type | Default  |
+|---|---|---|---|
+| ```severity``` | Severity to ignore, will filter from the result and return the correct exit code | ```String```  | ```info```  |
+| ```json``` | Weither to return a JSON or Table | ```Boolean```  | ```false```  |
+| ```ignore-dev``` | Weither to ignore dev-dependencies  | ```Boolean```  | ```false``` |
+| ```whitelist``` | List of module names to ignore, comma-separated | ```String``` | |
+
+### Examples
+
+Outputs a JSON but excluding debug
+```
+audit-check --json --whitelist debug
+```
+
+Outputs a JSON but only showing high or greater severity
+```
+audit-check --severity high --json
+```
+
+Outputs a table with only low and higher severity, excludes debug and only from production dependencies
+```
+audit-check --severity low --whitelist debug --ignore-dev
 ```
--- test/audit_tests.js
+++ test/audit_tests.js
@@ -1,311 +1,139 @@
 const assert = require('assert');
 const fs = require('fs');
+const _ = require('lodash');
 
 const Audit = require('../lib/audit.js');
 
 describe('Audit Tests', function () {
-  describe('None --json --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "");
 
-      let data = fs.readFileSync(__dirname + '/data/none.json');
-      this.audit.load(data)
-    })
+  describe('data tests', function () {
+    const testCases = [
+      {
+        args: { file: 'none.json', severity: 'low', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 0, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'single-low.json', severity: 'low', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 1, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'single-low.json', severity: 'low', json: false },
+        expected: { code: 1, metadata: { info: 0, low: 1, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'single-low.json', severity: 'low', json: true, whitelist: 'uglify-js' },
+        expected: { code: 0, metadata: { info: 0, low: 0, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 3, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true, whitelist: 'debug' },
+        expected: { code: 1, metadata: { info: 0, low: 2, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true, whitelist: 'non-existant' },
+        expected: { code: 1, metadata: { info: 0, low: 3, moderate: 0, high: 0, critical: 0 } }
+      },
+      {
+        args: { file: 'many.json', severity: 'info', json: true },
+        expected: { code: 1, metadata: { info: 1, low: 1, moderate: 1, high: 1, critical: 1 } }
+      },
+      {
+        args: { file: 'many.json', severity: 'low', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 1, moderate: 1, high: 1, critical: 1 } }
+      },
+      {
+        args: { file: 'many.json', severity: 'moderate', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 0, moderate: 1, high: 1, critical: 1 } }
+      },
+      {
+        args: { file: 'many.json', severity: 'high', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 0, moderate: 0, high: 1, critical: 1 } }
+      },
+      {
+        args: { file: 'many.json', severity: 'critical', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 0, moderate: 0, high: 0, critical: 1 } }
+      },
+      {
+        args: { file: 'no-critical.json', severity: 'high', json: true },
+        expected: { code: 1, metadata: { info: 0, low: 0, moderate: 0, high: 1, critical: 0 } }
+      },
+      {
+        args: { file: 'no-critical.json', severity: 'critical', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 0, moderate: 0, high: 0, critical: 0 } }
+      }
+    ];
 
-    it('getCode() should return 0', () => {
-      assert.equal(this.audit.getCode(), 0);
+    _.forEach(testCases, (test) => {
+      describe(`${test.args.file} - ${test.args.severity} ${test.args.whitelist ? test.args.whitelist : ''}`, function () {
+        before(() => {
+          this.audit = new Audit(test.args.severity, test.args.json, test.args.whitelist);
+
+          let data = fs.readFileSync(__dirname + '/data/' + test.args.file);
+          this.audit.load(data);
+        });
+
+        it('getCode() should return ' + test.expected.code, () => {
+          assert.equal(this.audit.getCode(), test.expected.code);
+        });
+
+        if (test.args.json) {
+          it('get() should have correct metadata', () => {
+            const data = JSON.parse(this.audit.get());
+
+            assert.equal(data.metadata.info, test.expected.metadata.info);
+            assert.equal(data.metadata.low, test.expected.metadata.low);
+            assert.equal(data.metadata.moderate, test.expected.metadata.moderate);
+            assert.equal(data.metadata.high, test.expected.metadata.high);
+            assert.equal(data.metadata.critical, test.expected.metadata.critical);
+          });
+        } else {
+          it('get() should have a value', () => {
+            const data = this.audit.get();
+
+            assert.ok(data);
+          });
+        }
+      });
     });
-
-    it('get() should have 0 metadata', () => {
-      let data = JSON.parse(this.audit.get());
-  
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
   });
 
-  describe('Single-Low --json --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "");
+  describe('summary tests', function() {
+    const testCases = [
+      {
+        args: { file: 'single-low.json', severity: 'low', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 1, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true },
+        expected: { code: 0, metadata: { info: 0, low: 3, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      },
+      {
+        args: { file: 'many-low.json', severity: 'low', json: true, whitelist: 'debug' },
+        expected: { code: 0, metadata: { info: 0, low: 2, moderate: 0, high: 0, critical: 0 } },
+        color: false
+      }
+    ];
 
-      let data = fs.readFileSync(__dirname + '/data/single-low.json');
-      this.audit.load(data)
-    })
+    _.forEach(testCases, (test) => {
+      describe(`${test.args.file} - ${test.args.severity} ${test.args.whitelist ? test.args.whitelist : ''}`, function () {
+        before(() => {
+          this.audit = new Audit(test.args.severity, test.args.json, test.args.whitelist);
 
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
+          let data = fs.readFileSync(__dirname + '/data/' + test.args.file);
+          this.audit.load(data);
+        });
+
+        it('getSummary() should have correct summary', () => {
+          const data = this.audit.getSummary(test.color);
+
+          assert.equal(data, `Found ${test.expected.metadata.info} info, ${test.expected.metadata.low} low, ${test.expected.metadata.moderate} moderate, ${test.expected.metadata.high} high, ${test.expected.metadata.critical} critical vulnerabilities.`);
+        });        
+      });
     });
-
-    it('get() should have 1 low', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 1);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
-  });
-
-  describe('Single-Low --json --severity low --whitelist', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "uglify-js");
-
-      let data = fs.readFileSync(__dirname + '/data/single-low.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 0', () => {
-      assert.equal(this.audit.getCode(), 0);
-    });
-
-    it('get() should have 1 low', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
-  });
-
-
-  describe('Single-Low --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', false, "");
-
-      let data = fs.readFileSync(__dirname + '/data/single-low.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-  });
-
-  describe('None --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', false, "");
-
-      let data = fs.readFileSync(__dirname + '/data/none.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 0', () => {
-      assert.equal(this.audit.getCode(), 0);
-    });
-    
-  });
-
-  describe('Many-Low --json --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many-low.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have 3 low', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 3);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
-  });
-
-  describe('Many-Low --json --severity low --whitelist', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "debug");
-
-      let data = fs.readFileSync(__dirname + '/data/many-low.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have 3 low', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 2);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
-  });
-
-  describe('Many --json --severity info', function () {
-    before(() => {
-      this.audit = new Audit('info', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 1);
-      assert.equal(data.metadata.low, 1);
-      assert.equal(data.metadata.moderate, 1);
-      assert.equal(data.metadata.high, 1);
-      assert.equal(data.metadata.critical, 1);
-    })
-  });
-
-  describe('Many --json --severity low', function () {
-    before(() => {
-      this.audit = new Audit('low', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 1);
-      assert.equal(data.metadata.moderate, 1);
-      assert.equal(data.metadata.high, 1);
-      assert.equal(data.metadata.critical, 1);
-    })
-  });
-
-  describe('Many --json --severity moderate', function () {
-    before(() => {
-      this.audit = new Audit('moderate', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 1);
-      assert.equal(data.metadata.high, 1);
-      assert.equal(data.metadata.critical, 1);
-    })
-  });
-
-  describe('Many --json --severity high', function () {
-    before(() => {
-      this.audit = new Audit('high', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 1);
-      assert.equal(data.metadata.critical, 1);
-    })
-  });
-
-  describe('Many --json --severity critical', function () {
-    before(() => {
-      this.audit = new Audit('critical', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/many.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 1);
-    })
-  });
-
-  describe('No Critical --json --severity critical', function () {
-    before(() => {
-      this.audit = new Audit('critical', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/no-critical.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 0);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 0);
-      assert.equal(data.metadata.critical, 0);
-    })
-  });
-
-  describe('No Critical --json --severity high', function () {
-    before(() => {
-      this.audit = new Audit('high', true, "");
-
-      let data = fs.readFileSync(__dirname + '/data/no-critical.json');
-      this.audit.load(data)
-    })
-
-    it('getCode() should return 1', () => {
-      assert.equal(this.audit.getCode(), 1);
-    });
-
-    it('get() should have many', () => {
-      let data = JSON.parse(this.audit.get());
-
-      assert.equal(data.metadata.info, 0);
-      assert.equal(data.metadata.low, 0);
-      assert.equal(data.metadata.moderate, 0);
-      assert.equal(data.metadata.high, 1);
-      assert.equal(data.metadata.critical, 0);
-    })
   });
 });
